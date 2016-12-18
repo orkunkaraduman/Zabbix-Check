9af8491e0a543f03d39c507ba2c29e7c68be6d8f
Orkun Karaduman <orkunkaraduman@gmail.com>
2016-12-18 15:45:23 +0300

    v1.02 init

--- lib/Zabbix/Check.pm
+++ lib/Zabbix/Check.pm
@@ -5,7 +5,7 @@ Zabbix::Check - Zabbix Agent system and service checks
 
 =head1 VERSION
 
-version 1.01
+version 1.02
 
 =head1 SYNOPSIS
 
@@ -116,7 +116,7 @@ BEGIN
 {
 	require Exporter;
 	# set the version for version checking
-	our $VERSION     = '1.01';
+	our $VERSION     = '1.02';
 	# Inherit from Exporter to export functions and variables
 	our @ISA         = qw(Exporter);
 	# Functions and variables which are exported by default
