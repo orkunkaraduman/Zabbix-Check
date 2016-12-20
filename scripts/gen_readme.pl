1b941df47d5216ef644a69266e51e41a0283d1a4
Orkun Karaduman <orkunkaraduman@gmail.com>
2016-12-20 11:29:36 +0300

    scripts PODs

--- scripts/gen_readme.pl
+++ scripts/gen_readme.pl
@@ -1,4 +1,17 @@
 #! /usr/bin/perl
+=head1 NAME
+
+gen_readme.pl - Generate README and MANIFEST
+
+=head1 VERSION
+
+version not defined
+
+=head1 SYNOPSIS
+
+Generate README and MANIFEST
+
+=cut
 use strict;
 use warnings;
 no warnings qw(qw utf8);
@@ -17,3 +30,35 @@ cwd($base);
 system('pod2markdown --html-encode-chars 1 lib/Zabbix/Check.pm > README.md');
 system('pod2text lib/Zabbix/Check.pm > README');
 system('git ls-files | grep -v "^\.gitignore" > MANIFEST');
+
+
+exit 0;
+__END__
+=head1 REPOSITORY
+
+B<GitHub> L<https://github.com/orkunkaraduman/Zabbix-Check>
+
+B<CPAN> L<https://metacpan.org/release/Zabbix-Check>
+
+=head1 AUTHOR
+
+Orkun Karaduman <orkunkaraduman@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2016  Orkun Karaduman <orkunkaraduman@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=cut
