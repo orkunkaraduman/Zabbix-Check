c672eec427ab43026a98cd75d918628f56f9f488
Orkun Karaduman <orkunkaraduman@gmail.com>
2016-12-20 11:24:50 +0300

    scripts dir && gen_readme.pl

--- scripts/gen_readme.pl
+++ scripts/gen_readme.pl
@@ -1,5 +1,17 @@
+#! /usr/bin/perl
 use strict;
 use warnings;
+no warnings qw(qw utf8);
+use v5.14;
+use utf8;
+use open qw(:std :locale);
+use Config;
+use FindBin;
+use Cwd;
+
+
+my $base = "${FindBin::Bin}/..";
+cwd($base);
 
 
 system('pod2markdown --html-encode-chars 1 lib/Zabbix/Check.pm > README.md');
